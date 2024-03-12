ac6078bcd83a9b9fd3a6f5dec09a6e23d817ac57
김경범 <gung2227@naver.com>
2024-03-12 16:57:21 +0900

    .

--- backend/ssafy_sec_proj/src/test/java/org/ssafy/ssafy_sec_proj/SsafySecProjApplicationTests.java
+++ backend/ssafy_sec_proj/src/test/java/org/ssafy/ssafy_sec_proj/SsafySecProjApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SsafySecProjApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
