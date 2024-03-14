fe591b01843fc23e7c32541c1df4acdd7856c493
Lim-SeungHwan <hwan7185@gmail.com>
2024-03-14 10:42:58 +0900

    Fix : api uri update

--- backend/ssafy_sec_proj/src/main/java/org/ssafy/ssafy_sec_proj/HelloController.java
+++ backend/ssafy_sec_proj/src/main/java/org/ssafy/ssafy_sec_proj/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/api/hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
