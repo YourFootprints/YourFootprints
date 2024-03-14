28dc089a43c8447fc9947703d5a5314f019af147
오세영 <osy9536@naver.com>
2024-03-14 17:50:01 +0900

    Init : 유저 관련 entity 정의
    추천 유저, 찜산 entity 개발

---
+++ backend/ssafy_sec_proj/src/main/java/org/ssafy/ssafy_sec_proj/users/entity/RecUsers.java
@@ -0,0 +1,59 @@
+package org.ssafy.ssafy_sec_proj.users.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.ssafy.ssafy_sec_proj._common.entity.BaseTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+// DB 테이블명이 클래스명과 다를 시 작성
+@Table(name = "rec_users")
+@SQLDelete(sql = "UPDATE rec_users set deleted_at = CONVERT_TZ(NOW(), 'UTC', 'Asia/Seoul') where id = ?")
+@Getter
+public class RecUsers extends BaseTime {
+
+    @Id
+    // auto_increment로 설정했다면 타입 설정할 것
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // 필드명이 다를 시 설정
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "cctv_num", nullable = false)
+    int cctvNum;
+
+    @Column(name = "convenience_num", nullable = false)
+    int convenienceNum;
+
+    @Column(name = "cafe_num", nullable = false)
+    int cafeNum;
+
+    @Column(name = "restaurant_num", nullable = false)
+    int restaurantNum;
+
+    @Column(name = "police_num", nullable = false)
+    int policeNum;
+
+    @Builder
+    private RecUsers(int cctvNum, int convenienceNum, int cafeNum, int restaurantNum, int policeNum) {
+        this.cctvNum = cctvNum;
+        this.convenienceNum = convenienceNum;
+        this.cafeNum = cafeNum;
+        this.restaurantNum = restaurantNum;
+        this.policeNum = policeNum;
+    }
+
+    public static RecUsers of(int cctvNum, int convenienceNum, int cafeNum, int restaurantNum, int policeNum) {
+        return builder()
+                .cctvNum(cctvNum)
+                .convenienceNum(convenienceNum)
+                .cafeNum(cafeNum)
+                .restaurantNum(restaurantNum)
+                .policeNum(policeNum)
+                .build();
+    }
+}
---
+++ backend/ssafy_sec_proj/src/main/java/org/ssafy/ssafy_sec_proj/users/entity/TrailsMidLikes.java
@@ -0,0 +1,47 @@
+package org.ssafy.ssafy_sec_proj.users.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.ssafy.ssafy_sec_proj._common.entity.BaseTime;
+import org.ssafy.ssafy_sec_proj.trail.entity.CustomTrails;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+// DB 테이블명이 클래스명과 다를 시 작성
+@Table(name = "trails_mid_likes")
+@SQLDelete(sql = "UPDATE trails_mid_likes set deleted_at = CONVERT_TZ(NOW(), 'UTC', 'Asia/Seoul') where id = ?")
+@Getter
+public class TrailsMidLikes extends BaseTime {
+
+    @Id
+    // auto_increment로 설정했다면 타입 설정할 것
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // 필드명이 다를 시 설정
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "users", nullable = false)
+    private User userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "custom_trails", nullable = false)
+    private CustomTrails trailsId;
+
+    @Builder
+    private TrailsMidLikes(User userId, CustomTrails trailsId) {
+        this.userId = userId;
+        this.trailsId = trailsId;
+    }
+
+    public static TrailsMidLikes of(User userId, CustomTrails trailsId) {
+        return builder()
+                .userId(userId)
+                .trailsId(trailsId)
+                .build();
+    }
+}
