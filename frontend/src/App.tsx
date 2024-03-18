310fb09c23d18b640561d80c1bce4cf6deb560fe
김경범 <gung2227@naver.com>
2024-03-18 17:30:30 +0900

    init: router 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,45 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
+import LoginPage from "./pages/Login/LoginPage";
+import HomePage from "./pages/Main/HomePage";
+
+
+const router = createBrowserRouter ([
+  // 메인 홈페이지
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  // 로그인
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+  // 추가 정보 입력
+  // {
+  //   path: '/signup',
+  //   element: <자기 페이지>,
+  // },
+  // 프로필
+  // {
+  //   path: '/profile',
+  //   element: <자기 페이지>,
+  // },
+  // 산책로
+  // {
+  //   path: '/trails',
+  //   element: <자기 페이지>,
+  // },
+
+])
+
 
 function App() {
-  return <div> 프론트 시작</div>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
