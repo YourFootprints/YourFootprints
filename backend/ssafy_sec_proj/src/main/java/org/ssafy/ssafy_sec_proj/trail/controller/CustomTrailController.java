895e72da95729641017082c6f7562956a9edba34
안유나 <yunanash1234@gmail.com>
2024-03-19 13:41:17 +0900

    feat : 산책 기록 상세 API 개발
    token 설정 : 본인 기록만 확인할 수 있도록 설정
    
    S10P22D207-153 #done #commnet 닫기

--- backend/ssafy_sec_proj/src/main/java/org/ssafy/ssafy_sec_proj/trail/controller/CustomTrailController.java
+++ backend/ssafy_sec_proj/src/main/java/org/ssafy/ssafy_sec_proj/trail/controller/CustomTrailController.java
@@ -8,17 +8,17 @@ import org.ssafy.ssafy_sec_proj._common.response.MsgType;
 import org.ssafy.ssafy_sec_proj._common.response.ResponseUtils;
 import org.ssafy.ssafy_sec_proj._common.security.UserDetailsImpl;
 import org.ssafy.ssafy_sec_proj.trail.dto.response.CustomTrailDetailResponseDto;
-import org.ssafy.ssafy_sec_proj.trail.service.CustomTrailDetailService;
+import org.ssafy.ssafy_sec_proj.trail.service.CustomTrailService;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class CustomTrailController {
-    private final CustomTrailDetailService customTrailsDetailService;
+    private final CustomTrailService customTrailsDetailService;
 
     @GetMapping("/main/trails/{trails-id}/records")
-    public ApiResponseDto<CustomTrailDetailResponseDto> getCustomTrailsDetail(@AuthenticationPrincipal UserDetailsImpl userDetails,
+    public ApiResponseDto<CustomTrailDetailResponseDto> getCustomTrailDetail(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                                               @PathVariable("trails-id") Long trailsId) {
-        return ResponseUtils.ok(customTrailsDetailService.readCustomTrailDetail(trailsId), MsgType.SEARCH_CUSTOM_TRAIL_DETAIL_SUCCESSFULLY);
+        return ResponseUtils.ok(customTrailsDetailService.readCustomTrailDetail(userDetails.getUser(), trailsId), MsgType.SEARCH_CUSTOM_TRAIL_DETAIL_SUCCESSFULLY);
     }
 }
--- backend/ssafy_sec_proj/src/main/java/org/ssafy/ssafy_sec_proj/trail/service/CustomTrailService.java
+++ backend/ssafy_sec_proj/src/main/java/org/ssafy/ssafy_sec_proj/trail/service/CustomTrailService.java
@@ -1,24 +1,24 @@
 package org.ssafy.ssafy_sec_proj.trail.service;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.BadRequestException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.ssafy.ssafy_sec_proj._common.exception.CustomException;
 import org.ssafy.ssafy_sec_proj._common.exception.ErrorType;
 import org.ssafy.ssafy_sec_proj.trail.dto.response.CustomTrailDetailResponseDto;
 import org.ssafy.ssafy_sec_proj.trail.entity.CustomTrails;
-import org.ssafy.ssafy_sec_proj.trail.repository.CustomTrailsRepository;
+import org.ssafy.ssafy_sec_proj.trail.repository.CustomTrailRepository;
+import org.ssafy.ssafy_sec_proj.users.entity.User;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class CustomTrailDetailService {
-    private final CustomTrailsRepository customTrailsRepository;
+public class CustomTrailService {
+    private final CustomTrailRepository customTrailRepository;
 
     // 산책 기록 상세
-    public CustomTrailDetailResponseDto readCustomTrailDetail(Long trailsId) {
-        CustomTrails customTrails = customTrailsRepository.findById(trailsId).orElseThrow(
+    public CustomTrailDetailResponseDto readCustomTrailDetail(User user, Long trailsId) {
+        CustomTrails customTrails = customTrailRepository.findByIdAndUserId(trailsId, user).orElseThrow(
                 () -> new CustomException(ErrorType.NOT_FOUND_TRAIL)
         );
         CustomTrailDetailResponseDto responseDto = CustomTrailDetailResponseDto.of(customTrails.getTrailsName(), customTrails.getCreatedAt(),
