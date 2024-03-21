3c476f56c6e23b414f4a26e631fa241303671cb9
김경범 <gung2227@naver.com>
2024-03-21 14:02:07 +0900

    test: 실시간 지도 마커 및 동선 테스트

--- frontend/src/pages/Main/StartrunPage.tsx
+++ frontend/src/pages/Main/StartrunPage.tsx
@@ -1,32 +1,54 @@
-import { css } from "@emotion/react";
-import { useCallback, useEffect, useRef, useState } from "react";
-
-const mapcss = css({
-  width: "100%",
-  height: "432px",
-  border: "1px solid red",
-});
+import { useEffect, useState } from "react";
+import Mab from "@/components/@common/Map";
+import { CircularProgress } from "@mui/material";
 
 export default function StartrunPage() {
-  const kakaomap = useRef<HTMLDivElement>(null);
-  const [lat, setLat] = useState<number[]>([]);
-  const [map, setMap] = useState(null);
-  const [positionArr, setPositionArr] = useState([]);
+  const [location, setLocation] = useState({
+    center: {
+      lat: 33.450701,
+      lng: 126.570667,
+    },
+    isLoading: true,
+  });
+  const [locationList, setLocationList] = useState([]);
+  const [test, setTest] = useState(null);
+
+  const onTest = (value) => {
+    setTest(value);
+  };
 
   useEffect(() => {
     if ("geolocation" in navigator) {
       // Geolocation API를 사용할 수 있는 경우
-      navigator.geolocation.getCurrentPosition(
+      navigator.geolocation.watchPosition(
         (position) => {
           // 위치 정보를 가져오는데 성공한 경우
           // position 객체에서 좌표 정보를 가지고 옵니다
-          const lats = position.coords.latitude; // 위도
-          const lons = position.coords.longitude; // 경도
-          setLat([lats, lons]);
+          const lat = position.coords.latitude; // 위도
+          const lng = position.coords.longitude; // 경도
+          setLocation((pre) => ({
+            ...pre,
+            center: {
+              lat: lat,
+              lng: lng,
+            },
+            isLoading: false,
+          }));
+          setLocationList((pre: any) => {
+            return [...pre, new kakao.maps.LatLng(lat, lng)];
+          });
         },
         (error) => {
           // 위치 정보를 가져오는데 실패한 경우`
           console.log(error);
+        },
+        {
+          // 옵션: 높은 정확도의 위치 정보를 요청할 수 있음
+          enableHighAccuracy: true,
+          // 옵션: 위치 정보가 캐시되는 최대 시간
+          maximumAge: 1000,
+          // 옵션: 위치 정보 요청의 최대 대기 시간
+          timeout: 2000,
         }
       );
     } else {
@@ -35,68 +57,98 @@ export default function StartrunPage() {
     }
   }, []);
 
-  useEffect(() => {
-    if (window.kakao && window.kakao.maps) {
-      if (lat.length > 0) {
-        const options = {
-          //지도를 생성할 때 필요한 기본 옵션
-          center: new window.kakao.maps.LatLng(lat[0], lat[1]), //지도의 중심좌표.
-          level: 3, //지도의 레벨(확대, 축소 정도)
-        };
-        console.log(lat, "lat");
-        const maps = new window.kakao.maps.Map(kakaomap.current, options); //지도 생성 및 객체 리턴
-        setMap(maps);
-      }
-    }
-  }, [lat]);
+  // // 라인을 그리는 함수
+  // const makeLine = useCallback(
+  //   (position: any) => {
+  //     const linePath = position;
 
-  // 라인을 그리는 함수
-  const makeLine = useCallback(
-    (position: any) => {
-      const linePath = position;
+  //     const polyline = new window.kakao.maps.Polyline({
+  //       path: linePath,
+  //       strokeWeight: 5,
+  //       strokeColor: "#FFAE00",
+  //       strokeOpacity: 0.7,
+  //       strokeStyle: "solid",
+  //     });
 
-      const polyline = new window.kakao.maps.Polyline({
-        path: linePath,
-        strokeWeight: 5,
-        strokeColor: "#FFAE00",
-        strokeOpacity: 0.7,
-        strokeStyle: "solid",
-      });
+  //     // 지도에 선을 표시합니다
+  //     polyline.setMap(map);
+  //   },
+  //   [map]
+  // );
 
-      // 지도에 선을 표시합니다
-      polyline.setMap(map);
-    },
-    [map]
+  // // 라인을 그리기 위한 좌표 배열을 만들어주는 함수
+  // const setLinePathArr = (position: GeolocationPosition) => {
+  //   const moveLatLon = new window.kakao.maps.LatLng(
+  //     position.coords.latitude,
+  //     position.coords.longitude
+  //   );
+  //   console.log(moveLatLon, "move");
+  //   const newPosition = positionArr.concat(moveLatLon); // positionArr는 kakao.maps.LatLng 객체의 배열이어야 합니다.
+  //   console.log(newPosition);
+  //   setPositionArr(newPosition);
+
+  //   // 라인을 그리는 함수 호출
+  //   makeLine(newPosition); // 수정된 호출 방식
+  // };
+
+  // useEffect(() => {
+  //   // map이 변경될 시 확인하고 map이 존재하면 5초마다 현재 위치를 가져오는 함수를 실행
+  //   if (map) {
+  //     const interval = setInterval(() => {
+  //       navigator.geolocation.getCurrentPosition(setLinePathArr);
+  //       console.log("실행중");
+  //     }, 5000);
+
+  //     return () => {
+  //       clearInterval(interval);
+  //     };
+  //   }
+  // }, [map]);
+
+  // 마커가 표시될 위치입니다
+  const markerPosition = new kakao.maps.LatLng(
+    location.center.lat,
+    location.center.lng
   );
 
-  // 라인을 그리기 위한 좌표 배열을 만들어주는 함수
-  const setLinePathArr = (position: GeolocationPosition) => {
-    const moveLatLon = new window.kakao.maps.LatLng(
-      position.coords.latitude,
-      position.coords.longitude
-    );
-    console.log(moveLatLon, "move");
-    const newPosition = positionArr.concat(moveLatLon); // positionArr는 kakao.maps.LatLng 객체의 배열이어야 합니다.
-    console.log(newPosition);
-    setPositionArr(newPosition);
+  // 마커를 생성합니다
+  const marker = new kakao.maps.Marker({
+    position: markerPosition,
+  });
 
-    // 라인을 그리는 함수 호출
-    makeLine(newPosition); // 수정된 호출 방식
-  };
+  // 마커가 지도 위에 표시되도록 설정합니다
+  marker.setMap(test);
 
-  useEffect(() => {
-    // map이 변경될 시 확인하고 map이 존재하면 5초마다 현재 위치를 가져오는 함수를 실행
-    if (map) {
-      const interval = setInterval(() => {
-        navigator.geolocation.getCurrentPosition(setLinePathArr);
-        console.log("실행중");
-      }, 5000);
+  const polyline = new kakao.maps.Polyline({
+    path: locationList, // 선을 구성하는 좌표배열 입니다
+    strokeWeight: 5, // 선의 두께 입니다
+    strokeColor: "#FFAE00", // 선의 색깔입니다
+    strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+    strokeStyle: "solid", // 선의 스타일입니다
+  });
 
-      return () => {
-        clearInterval(interval);
-      };
-    }
-  }, [map]);
+  polyline.setMap(test);
 
-  return <div ref={kakaomap} id="map" css={mapcss}></div>;
+  return (
+    <>
+      {location.isLoading ? (
+        <div
+          css={{
+            width: "100%",
+            height: "432px",
+          }}
+        >
+          <CircularProgress />
+        </div>
+      ) : (
+        <Mab
+          width="100%"
+          height="432px"
+          lat={location.center.lat}
+          lng={location.center.lng}
+          onTest={onTest}
+        />
+      )}
+    </>
+  );
 }
