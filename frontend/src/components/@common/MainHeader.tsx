b8712bebe53a91186718ccdcb6c9ecc7716db9d8
박규리 <dw05131@naver.com>
2024-03-21 13:24:59 +0900

    Feat/공통컴포넌트 : 메인헤더 생성
    하단탭 관련 메인헤더 생성
    
    S10P22D207-134 #done #comment 닫기

---
+++ frontend/src/components/@common/MainHeader.tsx
@@ -0,0 +1,33 @@
+import { css } from "@emotion/react";
+
+/*
+
+[REMOVE]
+title에 (하단)탭 제목 작성해주세요
+<MainHeader title={"테스트 페이지"} />
+
+*/
+
+interface MainHeaderProps {
+  title: string;
+}
+
+const MainHeader: React.FC<MainHeaderProps> = ({ title }) => {
+  const style = css({
+    width: "100%",
+    height: "60px",
+    fontSize: "20px",  // 높이와 글자크기는 고정
+    borderBottom: "1px solid black",
+    background: "white",
+    lineHeight: "60px",
+    top: "0",
+    position: "sticky",
+    zIndex: "10",
+  })
+
+
+  return (
+    <div css={style}>{title}</div>
+  )
+}
+export default MainHeader;
