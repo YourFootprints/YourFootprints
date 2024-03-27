da9c91e2c1b9c0e1931e32ab275ea9e596e6c4c4
박규리 <dw05131@naver.com>
2024-03-27 12:20:57 +0900

    Feat/기록 : 토글
    산책로 공개/비공개 토글 생성
    
    S10P22D207-250 #done #comment 닫기

---
+++ frontend/src/components/Record/PublicToggle.tsx
@@ -0,0 +1,98 @@
+import '@/index.css'
+import { css } from "@emotion/react";
+import { useState } from 'react';
+import TruePublic from "@/assets/Record/LockSimpleOpen.svg?react"
+import FalsePublic from "@/assets/Record/LockSimple.svg?react"
+
+interface PublicToggleProps {
+  isPublic: boolean;
+}
+
+const PublicToggle: React.FC<PublicToggleProps> = ({isPublic}) => {
+  const [isOpen, setIsOpen] = useState(isPublic);
+
+  const toggle = {
+    style: css({
+      width: "16.3vw",
+      height: "6.5vw",
+      borderRadius: "100px",
+      display: "flex",
+      alignItems: "center",
+      cursor: "pointer",
+      transition: "0.3s ease all",
+      position: "relative",
+      textAlign: "center",
+      "@media(min-width: 430px)": {
+        width: "70px",
+        height: "28px",
+      },
+    }),
+    public: css({
+      background: "var(--main-color)",
+    }),
+    private: css({
+      background: "var(--gray-200)",
+    })
+  }
+
+  const btn = {
+    style: css({
+      width: "5.1vw",
+      height: "5.1vw",
+      borderRadius: "100px",
+      background: "white",
+      marginLeft: "0.7vw",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      transition: "0.3s ease all",
+      "@media(min-width: 430px)": {
+        width: "22px",
+        height: "22px",
+        marginLeft: "3px",
+      },
+    }),
+    public: css({
+      transform: 'translateX(9.8vw)',
+      "@media(min-width: 430px)": {
+        transform: 'translateX(42px)',
+      }
+    }),
+  }
+
+  const publicType = {
+    style: css({
+      width: "9.8vw",
+      minWidth: "6.3vw",
+      fontSize: "2.8vw",
+      color: "white",
+      zIndex: "3",
+      position: "absolute",
+      "@media(min-width: 430px)": {
+        width: "42px",
+        minWidth: "28px",
+        fontSize: "12px",
+      },
+    }),
+    public: css({
+      left: "0",
+    }),
+    private: css({
+      left: "5.8vw",
+      "@media(min-width: 430px)": {
+        left: "25px",
+      }
+    })
+  }
+
+  return (
+    <div css={[toggle.style, (isOpen)?toggle.public:toggle.private]} onClick={()=>{setIsOpen(!isOpen)}}>
+      <div css={[btn.style,(isOpen)?btn.public:{}]}>
+        {isOpen?<TruePublic />:<FalsePublic />}
+      </div>
+      <div css={[publicType.style, (isOpen)?publicType.public:publicType.private]}>{isOpen?"공개":"비공개"}</div>
+    </div>
+  );
+};
+
+export default PublicToggle;
