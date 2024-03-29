61fdfd35567f97b8217db381f2b72218c68a6ec8
김경범 <gung2227@naver.com>
2024-03-29 14:10:55 +0900

    fix:스토어 수정

--- frontend/src/pages/Signup/Stepper.tsx
+++ frontend/src/pages/Signup/Stepper.tsx
@@ -68,8 +68,8 @@ export default function SignupStepper() {
       // Axios를 사용하여 put 요청을 보냅니다.
       axios
         .put(
-          // "http://localhost:8080/api/users/remain-info", // 로컬용
-          "https://j10d207.p.ssafy.io/api/users/remain-info", // 배포용
+          "http://localhost:8080/api/users/remain-info", // 로컬용
+          // "https://j10d207.p.ssafy.io/api/users/remain-info", // 배포용
           userData,
           config
         )
