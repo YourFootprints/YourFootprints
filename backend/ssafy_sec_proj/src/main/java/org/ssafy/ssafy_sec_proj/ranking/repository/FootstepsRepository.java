495cd3ff542aaff09f6747fd67cf963e81ef1d21
오세영 <osy9536@naver.com>
2024-03-31 13:19:41 +0900

    Feat : 내 발자국 찾기 기능 구현
    S10P22D207-145 #done #comment 닫기

--- backend/ssafy_sec_proj/src/main/java/org/ssafy/ssafy_sec_proj/ranking/repository/FootstepsRepository.java
+++ backend/ssafy_sec_proj/src/main/java/org/ssafy/ssafy_sec_proj/ranking/repository/FootstepsRepository.java
@@ -3,5 +3,8 @@ package org.ssafy.ssafy_sec_proj.ranking.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.ssafy.ssafy_sec_proj.ranking.entity.Footsteps;
 
+import java.util.List;
+
 public interface FootstepsRepository extends JpaRepository<Footsteps, Long> {
+    List<Footsteps> findAllByUserId(Long user);
 }
--- backend/ssafy_sec_proj/src/main/java/org/ssafy/ssafy_sec_proj/ranking/service/RankingService.java
+++ backend/ssafy_sec_proj/src/main/java/org/ssafy/ssafy_sec_proj/ranking/service/RankingService.java
@@ -3,14 +3,15 @@ package org.ssafy.ssafy_sec_proj.ranking.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.ssafy.ssafy_sec_proj.ranking.dto.responseDto.FootstepListResponseDto;
 import org.ssafy.ssafy_sec_proj.ranking.entity.Footsteps;
 import org.ssafy.ssafy_sec_proj.ranking.repository.FootstepsRepository;
-import org.ssafy.ssafy_sec_proj.trail.dto.response.CustomTrailDetailResponseDto;
 import org.ssafy.ssafy_sec_proj.trail.entity.SpotLists;
 import org.ssafy.ssafy_sec_proj.trail.repository.SpotListsRepository;
 import org.ssafy.ssafy_sec_proj.users.entity.User;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,14 +24,19 @@ public class RankingService {
     private final SpotListsRepository spotListsRepository;
     private final FootstepsRepository footstepsRepository;
 
-    public CustomTrailDetailResponseDto myFootstep(User user) {
+    public List<FootstepListResponseDto> myFootstep(User user) {
 
+        List<Footsteps> myFootsteps = footstepsRepository.findAllByUserId(user.getId());
 
-        return null;
+        List<FootstepListResponseDto> ansDtos = new ArrayList<>();
+        for (Footsteps f : myFootsteps) {
+            ansDtos.add(FootstepListResponseDto.of(user.getId(), user.getUserName(), f.getVisitedNum(), f.getLatitude(), f.getLongitude()));
+        }
+        return ansDtos;
     }
 
     public void makeFootstep() {
-        footstepsRepository.deleteAll();
+        footstepsRepository.deleteAllInBatch();
         LocalTime fiveMinutesAgo = LocalTime.of( 0, 0, 5);
         List<SpotLists> spotLists = spotListsRepository.findByDurationGreaterThanEqual(fiveMinutesAgo);
         // la와 lo가 같은 위치에 대해 유저별로 몇 번 지나갔는지 카운트
@@ -44,7 +50,7 @@ public class RankingService {
         }
 
         // Footsteps 엔티티에 데이터 저장 (n명 이상이 지나간 위치만 저장)
-        int N = 1;
+        int N = 3;
         for (Map.Entry<String, Map<Long, Integer>> entry : locationCountMap.entrySet()) {
             int totalVisited = entry.getValue().values().stream().mapToInt(Integer::intValue).sum();
             if (totalVisited >= N) {
