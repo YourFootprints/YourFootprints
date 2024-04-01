99371866151a916d1315283647eda07a8c85ca96
박규리 <dw05131@naver.com>
2024-04-01 21:50:07 +0900

    Feat/랭킹 : 동네발자국 API
    S10P22D207-299 #done #comment 닫기

--- frontend/src/services/Ranking.ts
+++ frontend/src/services/Ranking.ts
@@ -5,3 +5,9 @@ export const getMyFootprint = async() => {
   const res = await axiosAuthRequest.get("/api/rankings/my-footsteps")
   return res.data.data;
 }
+
+// 동네 발자국
+export const getAroundFootprint = async() => {
+  const res = await axiosAuthRequest.get("/api/rankings/around-footsteps")
+  return res.data.data;
+}
