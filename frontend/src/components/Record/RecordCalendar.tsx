a16a68f17fe3b8299ad72e86dcd8b7627b274218
박규리 <dw05131@naver.com>
2024-04-01 15:10:50 +0900

    Feat/기록 : 캘린더 목록 API 연결
    S10P22D207-296 #done #comment 닫기

--- frontend/src/components/Record/RecordCalendar.tsx
+++ frontend/src/components/Record/RecordCalendar.tsx
@@ -1,55 +1,66 @@
-import { useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
 import { css } from '@emotion/react';
 import './RecordCalendar.css'
-import dayjs from "dayjs";
+import dayjs from 'dayjs';
+import Calendar from 'react-calendar';
+import { useContext } from 'react';
+import { CalendarContext } from '@/pages/Record/RecordCalendarPage';
 
-type ValuePiece = Date | null;
-type Value = ValuePiece | [ValuePiece, ValuePiece];
+const RecordCalendar = () => {
+  const {
+    // clickDate, 
+    setClickDate, 
+    trailDate, 
+    // viewYear, 
+    setViewYear, 
+    // viewMonth, 
+    setViewMonth,
+  } = useContext(CalendarContext);
 
-function RecordCalendar() {
-  const [value, onChange] = useState<Value>(new Date());
+  const changeViewDate = ({activeStartDate}:{activeStartDate: Date|null}) => {
+    activeStartDate && setViewYear(activeStartDate?.getFullYear());
+    activeStartDate && setViewMonth(activeStartDate?.getMonth()+1);
+  }
 
   return (
-    <div css={style.box}>
-      <Calendar 
-        css={calendar} 
-        formatDay={(_locale, date) => dayjs(date).format("D")}
-        onChange={onChange} 
+    <div css={style}>
+      <Calendar
+        formatDay={(_locale, date) => dayjs(date).format('D')}
+        formatYear={(_locale, date) => dayjs(date).format('YYYY')}
+        formatMonthYear={(_locale, date) => dayjs(date).format('YYYY. MM')}
+        calendarType="gregory"        // 일월화수목금토 순서
+        showNeighboringMonth={false}  // 이전달 월말, 다음달 월초 날짜 보여주기 x
+        next2Label={null}             // 연도 (앞으로) 이동 x
+        prev2Label={null}             // 연도 (뒤로  ) 이동 x
+        onClickDay={(value) => {setClickDate(value)}}
         locale="en-US"
-        value={value} 
+        minDetail="month"
+        maxDetail="month"
+        onActiveStartDateChange={changeViewDate}
+        // onActiveStartDateChange={(e)=>{e.activeStartDate}}
+
+        tileContent={({ date }) => {
+          // FIXME const vs let
+          // eslint-disable-next-line prefer-const
+          let html: JSX.Element[] = [];
+          if (trailDate.find(x => x === dayjs(date).format('YYYY-M-D'))) {
+            html.push(<div key={dayjs(date).format('YYYY-M-D')} className='react-calendar__tile--hasActive' />);
+          }
+          return html;
+        }}
       />
     </div>
   );
-}
+};
 
-const style = {
-  box: css({
-    // width: "88%",
-    margin: "4.6vw 6vw 6.5vw 6vw",
-    cursor: "pointer",
-    borderRadius: "10px",
-    boxSizing: "border-box",
-    '@media(min-width: 430px)': {
-      margin: "20px 26px 28px 26px",
-    }
-  })
-}
-
-const calendar = css({
-  width: "100%",
-  // '.react-calendar': {
-  //   border: 'none',
-  // }
-})
-
-// const calendar = css`
-//   width: 100%;
-
-//   .react-calendar {
-//     border: none;
-//   }
-// `
+const style = css`
+  width: 100%;
+  margin-bottom: 4.6vw;
+  display: flex;
+  justify-content: center;
+  position: relative;
+  @media(min-width: 430px) {
+    margin-bottom: 20px;
+  };
+`
 
 export default RecordCalendar;
