7d570434b1636d338399a76504cf13a4deb7596e
안유나 <yunanash1234@gmail.com>
2024-04-02 09:45:20 +0900

    feat: 메인 페이지 API 개발

--- backend/ssafy_sec_proj/src/main/java/org/ssafy/ssafy_sec_proj/trail/service/UserTrailsRecordService.java
+++ backend/ssafy_sec_proj/src/main/java/org/ssafy/ssafy_sec_proj/trail/service/UserTrailsRecordService.java
@@ -14,8 +14,8 @@ import org.ssafy.ssafy_sec_proj.users.entity.User;
 import org.ssafy.ssafy_sec_proj.users.repository.TrailsMidLikesRepository;
 import org.ssafy.ssafy_sec_proj.users.repository.UserRepository;
 
+import org.springframework.http.HttpHeaders;
 import java.time.LocalDateTime;
-import java.time.temporal.WeekFields;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,7 +40,7 @@ public class UserTrailsRecordService {
         int accumulatedCalorie = 0;
         List<RecordResponseDto> aroundTrailsRecommend = new ArrayList<>();
 
-        // 산책 기록 없으면 => 아직 산책 기록이 없어용, 좋아요순 정렬
+        // 산책 기록 없으면 => 아직 산책 기록이 없어요, 좋아요순 정렬
         if (customeTrilsList.isEmpty()) {
             aroundTrailsRecommend.addAll(customTrailsRepository.findAllByIsPublicIsTrueAndDeletedAtIsNullOrderByLikeNumDesc()
                     .orElse(null).stream()
@@ -54,20 +54,44 @@ public class UserTrailsRecordService {
                             checkIsLike(c.getUserId(), c)
                     )).toList());
         } else {
-            // 이번 달 산책 기록이 있는지 체크 => 있으면 더해주기
+            // 이번 달 산책 기록이 체크 => 있으면 더한 값 반환
             int currentMonth = LocalDateTime.now().getMonthValue();
             List<CustomTrails> currentList = customTrailsRepository.findAllCustomTrailsByCreatedAtAndDeletedAtIsNull(currentMonth).orElse(null);
 
+            // 이번 달 산책 기록이 있다면
             if (!currentList.isEmpty()) {
+                // 순회하면서 더해주기
+                int totalRuntime = 0 ;
+                for (CustomTrails customTrail : currentList) {
+                    totalRuntime += transferRuntimeToSec(customTrail.getRuntime());
+                    accumulatedDistance  += customTrail.getDistance();
+                    accumulatedCalorie += customTrail.getCalorie();
+                }
 
-            }
-            // 순회하면서 더해주기
-            for (CustomTrails customTrail : customeTrilsList) {
-                System.out.println(customTrail.getDistance());
+                int hours = totalRuntime / 3600;
+                int minutes = (totalRuntime % 3600) / 60;
+                int seconds = totalRuntime % 60;
+
+                // 시간을 문자열로 변환
+                accumulatedWalkingTime = String.format("%01d:%02d:%02d", hours, minutes, seconds);
+
             }
             // 추천 목록 가져오기 : 군집 번호로 가져와서 그 중 좋아요 높은 순
+            /*
+            fastapi 연결
+            cafe_num : int
+            cctv_num :  int
+            convenience_num : int
+            police_num : int
+            restaurant_num : int
+            gender : str
+            age_range :str
+            prefer_duration_e : int
+            prefer_duration_s : int
+            sum_num : int
+             */
+            HttpHeaders headers =  new HttpHeaders();
         }
-
 
 
         MainResponseDto responseDto = MainResponseDto.of(user.getKakaoProfileImg(), user.getNickName(),
@@ -81,6 +105,13 @@ public class UserTrailsRecordService {
         String[] times = runtime.split(":");
         return Integer.parseInt(times[0]) * 60 + Integer.parseInt(times[1]);
     }
+
+    // runtime 초 단위로 변환하는 메서드
+    public int transferRuntimeToSec(String runtime) {
+        String[] times = runtime.split(":");
+        return Integer.parseInt(times[0]) * 3600 + Integer.parseInt(times[1]) * 60 + + Integer.parseInt(times[2]);
+    }
+
 
     // 좋아요 여부 판단 : id와 user로 midlikes에 있는지 체크
     public boolean checkIsLike(User user, CustomTrails customTrails) {
