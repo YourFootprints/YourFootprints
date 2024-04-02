b57c1068c3d387328cf4b64c5178fad67defde68
jinyoung <kimyoung100@naver.com>
2024-04-02 20:57:57 +0900

    Feat: 로딩창만들기, 로그인창 css 수정

--- frontend/src/assets/lotties/lottiesOptions.ts
+++ frontend/src/assets/lotties/lottiesOptions.ts
@@ -1,11 +1,31 @@
 import walking from "@/assets/lotties/walking.json";
 import loginwalking from "@/assets/lotties/loginwalking.json";
 import print from "@/assets/lotties/foot.json";
+import loading from "@/assets/lotties/loading.json";
+import loadingfoot from "@/assets/lotties/loadingfoot.json";
 
 export const walkingOptions = {
   loop: true,
   autoplay: true,
   animationData: walking,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+
+export const loadingOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: loading,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+
+export const loadingfootOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: loadingfoot,
   rendererSettings: {
     preserveAspectRatio: "xMidYMid slice",
   },
